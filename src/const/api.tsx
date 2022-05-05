4f8b0f903e8f2463457abb4446dc26df30fc4777
小翼 <zhijun@furry.top>
2022-05-06 00:40:18 +0400

    调整API地址

--- src/const/api.tsx
+++ src/const/api.tsx
@@ -1,4 +1,4 @@
 export const API_SPLATINK: string = 'https://splatoon2.ink/data'
-export const API_BACKEND: string = process.env.API_BACKEND || 'http://127.0.0.1:5000'
+export const API_BACKEND: string = process.env.REACT_APP_API_BACKEND || 'http://127.0.0.1:5000'
 export const RECORDNUM: string = process.env.REACT_APP_RECORDNUM || 'not_set'
 export const BUILD: string = process.env.REACT_APP_BUILD || 'dev_build'
